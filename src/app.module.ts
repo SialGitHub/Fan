cc3798720e477e2f28f4890b14396b26b04cc6fe
gill <filger1515@gmail.com>
2020-09-07 18:00:32 +0300

    build(components): fix
    fix
    
    МЕТА ДАННЫЕ: test-6

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController} from "./app.controller";
 import { AppService} from "./app.service";
 
-const username = process.env.POSTGRES_USER;
-const password = process.env.POSTGRES_PASSWORD;
-
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username,
-      password,
-      database: 'postgres',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
