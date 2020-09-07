2e375c04620f7a61ca421a22c29046de671ae475
gill <filger1515@gmail.com>
2020-09-07 15:48:57 +0300

    build(components): setyp build docker, nest and postgres
    setup env and docker setup database and typeorm
    
    МЕТА ДАННЫЕ: TESTCLEAR-2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController} from "./app.controller";
+import { AppService} from "./app.service";
+
+const username = process.env.POSTGRES_USER;
+const password = process.env.POSTGRES_PASSWORD;
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username,
+      password,
+      database: 'postgres',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
