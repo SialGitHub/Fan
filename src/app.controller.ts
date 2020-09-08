84c1e51496812e2e072827d0bab7fd1b8bbfa9d0
gill <filger1515@gmail.com>
2020-09-08 13:41:24 +0300

    feat(components): auth
    auth
    
    МЕТА ДАННЫЕ: TESTCLEAR=8

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Request, Get, Post, UseGuards } from "@nestjs/common";
 import { AppService } from './app.service';
+import { LocalAuthGuard } from "./auth/local-auth.guard";
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,
+              private authService: AuthService) {}
 
-  @Get()
+  @Get('/')
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/test')
+  getTest(): string{
+    return this.appService.getString();
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,11 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController} from "./app.controller";
 import { AppService} from "./app.service";
-
-const username = process.env.POSTGRES_USER;
-const password = process.env.POSTGRES_PASSWORD;
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username,
-      password,
-      database: 'postgres',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
-  ],
+  imports: [AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
